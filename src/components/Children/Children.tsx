49ad1db3d6db9a6829f60dfa4a8b332fba95bfd9
Angkit <angkithashamsa21@gmail.com>
2022-04-09 23:23:47 +0530

    created base folder

--- src/components/Children/Children.tsx
+++
@@ -1,6 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-const Children: React.FC<Props> = ({ children }) => <div>{children}</div>;
-
-export default Children;
--- src/components/Status/index.ts
+++
@@ -1,4 +0,0 @@
-import { memo } from "react";
-import Status from "./Status";
-
-export default memo(Status);
